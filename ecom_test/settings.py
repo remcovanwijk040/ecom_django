bebf1694a0da380142ef19418afa723a295eaafc
remcovanwijk040 <113189038+remcovanwijk040@users.noreply.github.com>
2023-12-04 23:38:43 +0100

    Update settings.py

--- ecom_test/settings.py
+++ ecom_test/settings.py
@@ -147,5 +147,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'djvonwick@gmail.com'
-EMAIL_HOST_PASSWORD = 'teok wtmx kkta vhbc'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
